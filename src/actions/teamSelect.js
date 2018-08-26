c21ec30c95693c5d6743df46c2cc32ec0d763652
Alex Harris <akharri8@ncsu.edu>
2018-08-26 14:03:36 -0400

    Added team divisions actions and reducer. This will be used to compare team stats within the same division. Also added D3 and an example D3 component that I will pass division information into in order to render a comparision chart

--- src/actions/teamSelect.js
+++ src/actions/teamSelect.js
@@ -1,12 +1,64 @@
 // nba package does not have an @types file so using standard JS for this file
 
 const nba = require('nba');
-
+console.log(nba);
 module.exports = {
-  getTeams
+  getTeams,
+  getDivisions
 }
 
 function getTeams() {
-    const teams = nba.teams;
-    return {type: 'GET_TEAMS', payload: teams};
+  const teams = nba.teams;
+  return {type: 'GET_TEAMS', payload: teams};
+}
+
+
+// Hard coded the divisions because the way the API is set up I would need to make over
+// 30 requests and this will slow UI.
+function getDivisions() {
+  console.log('called');
+  const teamDivisions = {
+      Atlantic: [
+        'Boston Celtics', 
+        'New Jersey Nets',
+        'New York Knicks', 
+        'Philadelphia 76ers',  
+        'Toronto Raptors'
+      ],
+      Central: [
+        'Chicago Bulls', 
+        'Cleveland Cavaliers',
+        'Detroit Pistons', 
+        'Indiana Pacers',
+        'Milwaukee Bucks'
+      ],
+      Southeast: [
+        'Atlanta Hawks', 
+        'Charlotte Hornets', 
+        'Miami Heat', 
+        'Orlando Magic', 
+        'Washington Wizards'
+      ],
+      Northweat: [
+        'Denver Nuggets', 
+        'Minnesota Timberwolves', 
+        'Oklahoma City Thunder', 
+        'Porland Trail Blazers',
+        'Utah Jazz'
+      ],
+      Pacific: [
+        'Golden State Warriors', 
+        'Los Angeles Clippers', 
+        'Los Angeles Lakers', 
+        'Phoenix Suns',
+        'Sacramento Kings'],
+      Southwest: [
+        'Dallas Mavericks', 
+        'Houston Rockets', 
+        'Memphis Grizzlies', 
+        'New Orleans Pelicans', 
+        'San Antonio Spurs'
+      ]                 
+    }
+  return {type: 'GET_DIVISIONS', payload: teamDivisions};
 }
